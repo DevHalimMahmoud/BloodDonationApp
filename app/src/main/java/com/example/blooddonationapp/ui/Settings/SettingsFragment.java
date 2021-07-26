25037fee7c45bc029fa8cb08fab2a30c9d8ed552
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2021-07-26 18:19:45 +0200

    Update SettingsFragment.java

--- app/src/main/java/com/example/blooddonationapp/ui/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/blooddonationapp/ui/Settings/SettingsFragment.java
@@ -73,13 +73,13 @@ public class SettingsFragment extends Fragment {
                 AlertDialog.Builder builder = new AlertDialog.Builder(root.getContext());
                 builder.setTitle("Write Your Password To Change Email");
 
-// Set up the input
+                // Set up the input
                 final EditText input = new EditText(root.getContext());
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
                 input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 builder.setView(input);
 
-// Set up the buttons
+                // Set up the buttons
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
