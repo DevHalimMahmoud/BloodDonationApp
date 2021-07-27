993f9f7c66e8f295fcc2809157f4191f3328bc60
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2021-07-27 22:38:02 +0200

    Update HomeFragment.java

--- app/src/main/java/com/example/blooddonationapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/blooddonationapp/ui/home/HomeFragment.java
@@ -10,10 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.example.blooddonationapp.FirebaseAuthSingleton;
 import com.example.blooddonationapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,7 +24,7 @@ public class HomeFragment extends Fragment {
 
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseAuthSingleton mAuth = FirebaseAuthSingleton.INSTANCE;
 
     TextView name, numberOfDonations, bloodType, donate, request_donation;
 
@@ -32,10 +32,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        name = root.findViewById(R.id.textView9);
-        numberOfDonations = root.findViewById(R.id.textView14);
-        bloodType = root.findViewById(R.id.textView11);
-        DocumentReference docRef = db.collection("users").document(Objects.requireNonNull(mAuth.getUid()));
+        dataBinding(root);
+        requestUserInfo();
+        handleUserClicks(root);
+
+
+        return root;
+    }
+
+    private void handleUserClicks(View root) {
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Navigation.findNavController(root).navigate(R.id.action_nav_home_to_nav_Donate);
+            }
+        });
+
+        request_donation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.action_nav_home_to_nav_Request_Donation);
+            }
+        });
+    }
+
+    private void requestUserInfo() {
+        DocumentReference docRef = db.collection("users").document(Objects.requireNonNull(mAuth.getInstance().getUid()));
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -47,28 +71,19 @@ public class HomeFragment extends Fragment {
                         numberOfDonations.setText(Objects.requireNonNull(document.get("num_of_donation")).toString());
                         bloodType.setText(Objects.requireNonNull(document.get("blood_type")).toString());
                     }
-                    donate = root.findViewById(R.id.donate);
-                    donate.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
 
-
-                            Navigation.findNavController(root).navigate(R.id.action_nav_home_to_nav_Donate);
-                        }
-                    });
-                    request_donation = root.findViewById(R.id.request_donation);
-                    request_donation.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Navigation.findNavController(root).navigate(R.id.action_nav_home_to_nav_Request_Donation);
-                        }
-                    });
 
                 }
             }
         });
+    }
 
-        return root;
+    private void dataBinding(View root) {
+        name = root.findViewById(R.id.textView9);
+        numberOfDonations = root.findViewById(R.id.textView14);
+        bloodType = root.findViewById(R.id.textView11);
+        request_donation = root.findViewById(R.id.request_donation);
+        donate = root.findViewById(R.id.donate);
     }
 
 }
